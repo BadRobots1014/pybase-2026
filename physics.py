789018dc8b72e51334f9471ccff2bcf2cebc6665
CodedMasonry <109301662+CodedMasonry@users.noreply.github.com>
2025-12-15 18:54:24 -0500

    add basic swerve imports

--- physics.py
+++ physics.py
@@ -1,13 +1,32 @@
+from typing import TYPE_CHECKING
+
 from pyfrc.physics import core
 
-from .robot import Robot
+from swervepy.impl import CoaxialSwerveModule
+from swervepy.impl.motor import DummyCoaxialAzimuthComponent, DummyCoaxialDriveComponent
+
+# explicit importing causes errors, only needed for type checking
+if TYPE_CHECKING:
+    from robot import Robot
 
 
 class PhysicsEngine(core.PhysicsEngine):
-    def __init__(self, physics_controller, robot: Robot):
+    def __init__(self, physics_controller, robot: "Robot"):
         self.physics_controller = physics_controller
 
+        # Switch all swerve motors to fake components for sim
         self.swerve = robot.container.swerve
+        self.swerve._modules = tuple(
+            CoaxialSwerveModule(
+                drive=DummyCoaxialDriveComponent(),
+                azimuth=DummyCoaxialAzimuthComponent(),
+                placement=module.placement,
+            )
+            for module in self.swerve._modules
+        )
+
+        return
 
     def update_sim(self, now, tm_diff):
-        #self.physics_controller.drive(speeds, tm_diff)
+        # self.physics_controller.drive(speeds, tm_diff)
+        return
