ae6fe2b0c8a3e2171a993c50e1cf6fdea19bb5e1
CodedMasonry <109301662+CodedMasonry@users.noreply.github.com>
2025-12-15 18:28:48 -0500

    strip physics sim

--- physics.py
+++ physics.py
@@ -1,20 +1,13 @@
-import hal.simulation
-from pyfrc.physics import drivetrains
+from pyfrc.physics import core
+
+from .robot import Robot
 
 
-class PhysicsEngine:
-    def __init__(self, physics_controller):
+class PhysicsEngine(core.PhysicsEngine):
+    def __init__(self, physics_controller, robot: Robot):
         self.physics_controller = physics_controller
-        self.drivetrain = drivetrains.TwoMotorDrivetrain(
-            deadzone=drivetrains.linear_deadzone(0.2)
-        )
 
-        self.l_motor = hal.simulation.PWMSim(1)
-        self.r_motor = hal.simulation.PWMSim(2)
+        self.swerve = robot.container.swerve
 
     def update_sim(self, now, tm_diff):
-        l_motor = self.l_motor.getSpeed()
-        r_motor = self.r_motor.getSpeed()
-
-        speeds = self.drivetrain.calculate(l_motor, r_motor)
-        self.physics_controller.drive(speeds, tm_diff)
+        #self.physics_controller.drive(speeds, tm_diff)
